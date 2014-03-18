07d6929ee53f16a96b3fcc12ef5575b0e4e73521
Alexey Gavrilov <alexey.gavrilov@gmail.com>
2014-03-19 00:30:13 +0400

    fixed TaskTypeRequest

---
+++ SherpaDesk/Models/Request/TaskTypeRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SherpaDesk.Models.Request
+{
+    [DataContract]
+    public class TaskTypeRequest : ObjectBase
+    {
+        [DataMember(Name = "key"), Details]
+        public string Key { get; set; }
+
+        [DataMember(Name = "ticket"), Details]
+        public string Ticket { get; set; }
+
+        [DataMember(Name = "project"), Details]
+        public int ProjectId { get; set; }
+    }
+}
