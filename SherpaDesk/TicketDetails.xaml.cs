f9acfe117aab7d2af3e636d8129455196a9a6b79
artemkorzhavin <artem-itsolution@yandex.ru>
2014-03-27 13:02:52 +0400

    Workpad

--- SherpaDesk/TicketDetails.xaml.cs
+++ SherpaDesk/TicketDetails.xaml.cs
@@ -25,5 +25,40 @@ namespace SherpaDesk
         {
             SubjectDecorate.Height = SubjectLabel.ActualHeight;            
         }
+
+        private void ToggleWorkpad()
+        {
+            if (WorkpadEditBox.Visibility == Windows.UI.Xaml.Visibility.Collapsed)
+            {
+                WorkpadLabel.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                WorkpadEditBox.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                WorkpadSaveButton.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                WorkpadCancelButton.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                WorkpadButton.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            }
+            else
+            {
+                WorkpadLabel.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                WorkpadEditBox.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                WorkpadSaveButton.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                WorkpadCancelButton.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                WorkpadButton.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            }
+        }
+
+        private void WorkpadButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ToggleWorkpad();
+        }
+
+        private void WorkpadSaveButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ToggleWorkpad();
+        }
+
+        private void WorkpadCancelButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ToggleWorkpad();
+        }
     }
 }
