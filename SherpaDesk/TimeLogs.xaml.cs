2b05e93de4fc85db24a529cc94d268b4c60ded49
Alexey Gavrilov <alexey.gavrilov@gmail.com>
2014-10-27 11:06:54 +0300

    Inline Editing

--- SherpaDesk/TimeLogs.xaml.cs
+++ SherpaDesk/TimeLogs.xaml.cs
@@ -1,39 +1,55 @@
-﻿using System.Linq;
-using SherpaDesk.Common;
-using SherpaDesk.Models;
-using SherpaDesk.Models.Response;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Navigation;
-using System;
+﻿using SherpaDesk.Models.Response;
+using SherpaDesk.Models.ViewModels;
 using System.Collections.ObjectModel;
+using Telerik.UI.Xaml.Controls.Grid.Commands;
+using Windows.UI.Xaml.Navigation;
 
 namespace SherpaDesk
 {
     public sealed partial class TimeLogs : SherpaDesk.Common.LayoutAwarePage
     {
-        private ObservableCollection<TimeResponse> list;
-
         public TimeLogs()
         {
             this.InitializeComponent();
+
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            list = (ObservableCollection<TimeResponse>)e.Parameter;
-            UpdateGrid(list);
             base.OnNavigatedTo(e);
+
+            UpdateGrid((ObservableCollection<TimeResponse>)e.Parameter);
         }
 
         public void UpdateGrid(ObservableCollection<TimeResponse> list)
         {
-            TicketTimeGrid.ItemsSource = list;
-            TicketTimeGrid.UpdateLayout();
+            this.DataContext = new TimeLogsModel(list);
+        }
+    }
+
+    public class CustomCommitEditCommand : DataGridCommand
+    {
+        public CustomCommitEditCommand()
+        {
+            this.Id = CommandId.CommitEdit;
         }
 
-        private void EditButton_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        public override bool CanExecute(object parameter)
         {
+            return true;
+        }
 
+        public override void Execute(object parameter)
+        {
+            var context = parameter as EditContext;
+
+            this.Owner.CommandService.ExecuteDefaultCommand(CommandId.CommitEdit, context);
+
+            TimeResponse time = context.CellInfo.Item as TimeResponse;
+            if (time != null)
+            {
+                //TODO: update time in Sherpadesk API
+            }
         }
     }
 }
